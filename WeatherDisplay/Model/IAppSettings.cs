dc99fa628c14f36a5be9bc95e430cc60df8696ce
Thomas Galliker <thomas@bluewin.ch>
2022-02-27 10:26:35 +0100

    Reformat

--- WeatherDisplay/Model/IAppSettings.cs
+++ WeatherDisplay/Model/IAppSettings.cs
@@ -6,9 +6,13 @@ namespace WeatherDisplay.Model
     public interface IAppSettings
     {
         string Title { get; set; }
+
         CultureInfo CultureInfo { get; set; }
+
         bool IsDebug { get; set; }
+
         List<AppSettings.DisplaySetting> Displays { get; set; }
+
         List<Place> Places { get; set; }
     }
 }
