fc67ca45e4155deccff2614a894b7812df41ed96
Thomas Galliker <thomas@bluewin.ch>
2022-03-16 21:21:09 +0100

    Copy script to output folder

--- WeatherDisplay.Api/Scripts/update_weatherdisplay_api.sh
+++ WeatherDisplay.Api/Scripts/update_weatherdisplay_api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "PiWeatherStation [Version 1.0.0]"
+echo "PiWeatherStation Update Script [Version 1.0.0]"
 echo "(c) superdev gmbh. All rights reserved."
 echo ""
 
@@ -14,25 +14,17 @@ fi
 
 if($preRelease = true) then
   echo "Download latest version (stable or preview)..."
-  curl -L --output "$downloadFile" $(curl -s https://api.github.com/repos/thomasgalliker/PiWeatherStation/releases | grep browser_download_url | cut -d '"' -f 4 | head -n 1)
+  curl -L --output "$downloadFile" --progress-bar $(curl -s https://api.github.com/repos/thomasgalliker/PiWeatherStation/releases | grep browser_download_url | cut -d '"' -f 4 | head -n 1)
 else
   echo "Download latest stable version..."
-  curl -L --output "$downloadFile" $(curl -s https://api.github.com/repos/thomasgalliker/PiWeatherStation/releases/latest | grep browser_download_url | cut -d '"' -f 4)
+  curl -L --output "$downloadFile" --progress-bar $(curl -s https://api.github.com/repos/thomasgalliker/PiWeatherStation/releases/latest | grep browser_download_url | cut -d '"' -f 4)
 fi
-
-#targetDir="~/temp"
-#if [[ -d $targetDir ]]; then
-#    echo "$(targetDir) already  exists"
-#else
-#    echo "Creating $(targetDir)..."
-#    mkdir $targetDir
-#fi
 
 echo "Stopping weatherdisplay service..."
 sudo systemctl stop weatherdisplay.api.service
 
 echo "Updating weatherdisplay service..."
-unzip -q -o "$downloadFile" -d "../WeatherDisplay.Api"
+unzip -q -o "$downloadFile" -d ".."
 rm "$downloadFile"
 
 echo "Starting weatherdisplay service..."
@@ -41,3 +33,4 @@ sudo systemctl enable weatherdisplay.api.service
 sudo systemctl start weatherdisplay.api.service
 
 echo "Successfully finished"
+echo ""
