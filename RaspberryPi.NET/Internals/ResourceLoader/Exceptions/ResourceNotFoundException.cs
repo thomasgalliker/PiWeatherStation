fae1ed2c0a4c738293266e28600700cc5548de3c
Thomas Galliker <thomas@bluewin.ch>
2022-09-03 13:15:43 +0200

    Add resource loader

---
+++ RaspberryPi.NET/Internals/ResourceLoader/Exceptions/ResourceNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace RaspberryPi.Internals.ResourceLoader.Exceptions
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class ResourceNotFoundException : Exception
+    {
+        internal ResourceNotFoundException(string resourceFileName)
+            : base(string.Format("Resource ending with {0} not found.", resourceFileName))
+        {
+        }
+    }
+}
