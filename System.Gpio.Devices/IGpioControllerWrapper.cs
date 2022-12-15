d62c28b4cb678ccc80e8a7acb13e623e68d9e4ea
Thomas Galliker <thomas@bluewin.ch>
2022-12-15 07:15:31 +0100

    Cleanup

--- System.Gpio.Devices/IGpioControllerWrapper.cs
+++ System.Gpio.Devices/IGpioControllerWrapper.cs
@@ -5,10 +5,15 @@ namespace System.Gpio.Devices
     public interface IGpioController : IDisposable
     {
         bool IsPinModeSupported(int pinNumber, PinMode mode);
+
         void ClosePin(int pinNumber);
+
         void OpenPin(int pinNumber, PinMode mode);
+
         void RegisterCallbackForPinValueChangedEvent(int pinNumber, PinEventTypes eventTypes, PinChangeEventHandler callback);
+
         void UnregisterCallbackForPinValueChangedEvent(int pinNumber, PinChangeEventHandler callback);
+
         void Write(int pinNumber, PinValue pinValue);
     }
 }
