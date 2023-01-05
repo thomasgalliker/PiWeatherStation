81442a206678a25c22a0bbb33455ff8288f63830
Thomas Galliker <thomas@bluewin.ch>
2023-01-05 15:03:14 +0100

    Extract colors and add unit test

---
+++ DisplayService/Resources/Colors.cs
@@ -0,0 +1,23 @@
+﻿namespace DisplayService.Resources
+{
+    public static class Colors
+    {
+        public static string Black { get; } = "#000000";
+
+        public static string White { get; } = "#FFFFFF";
+        
+        public static string LightGray { get; } = "#FFD3D3D3";
+
+        public static string Red { get; } = "#FF0000";
+
+        public static string Green { get; } = "#00FF00";
+
+        public static string Blue { get; } = "#0000FF";
+
+        public static string Cyan { get; } = "##00FFFF";
+
+        public static string Magenta { get; } = "#FF00FF";
+
+        public static string Yellow { get; } = "#FFFF00";
+    }
+}
