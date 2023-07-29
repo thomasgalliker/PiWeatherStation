c95400f2d7d3e9894e080a8b9b914c0b88478831
Thomas Galliker <thomas@bluewin.ch>
2023-07-29 14:34:48 +0200

    Inherit documentation in interface

--- System.Gpio.Devices/Gpio/IGpioController.cs
+++ System.Gpio.Devices/Gpio/IGpioController.cs
@@ -1,17 +1,24 @@
 ﻿namespace System.Device.Gpio
 {
+    /// <inheritdoc cref="GpioController" />
     public interface IGpioController : IDisposable
     {
+        /// <inheritdoc cref="GpioController.IsPinModeSupported(int, PinMode)" />
         bool IsPinModeSupported(int pinNumber, PinMode mode);
 
+        /// <inheritdoc cref="GpioController.ClosePin(int)" />
         void ClosePin(int pinNumber);
 
+        /// <inheritdoc cref="GpioController.OpenPin(int, PinMode)" />
         void OpenPin(int pinNumber, PinMode mode);
 
+        /// <inheritdoc cref="GpioController.RegisterCallbackForPinValueChangedEvent(int, PinEventTypes, PinChangeEventHandler)" />
         void RegisterCallbackForPinValueChangedEvent(int pinNumber, PinEventTypes eventTypes, PinChangeEventHandler callback);
 
+        /// <inheritdoc cref="GpioController.UnregisterCallbackForPinValueChangedEvent(int, PinChangeEventHandler)" />
         void UnregisterCallbackForPinValueChangedEvent(int pinNumber, PinChangeEventHandler callback);
 
+        /// <inheritdoc cref="GpioController.Write(int, PinMode)" />
         void Write(int pinNumber, PinValue pinValue);
     }
 }
