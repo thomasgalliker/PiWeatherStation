a088170927c754a786f5c8dd8122ace93bf242c6
Thomas Galliker <thomas@bluewin.ch>
2023-08-06 16:50:05 +0200

    Fix unit tests

--- Tests/System.Gpio.Devices.Tests/Buttons/GpioButtonTests.cs
+++ Tests/System.Gpio.Devices.Tests/Buttons/GpioButtonTests.cs
@@ -57,11 +57,11 @@ namespace System.Gpio.Devices.Tests.Buttons
         }
 
         [Theory]
-        [InlineData(true, PinEventTypes.Falling, 1, 0)]
-        [InlineData(true, PinEventTypes.Rising, 0, 1)]
+        [InlineData(false, PinEventTypes.Falling, 0, 0)]
         [InlineData(false, PinEventTypes.Rising, 1, 0)]
-        [InlineData(false, PinEventTypes.Falling, 0, 1)]
-        public void ShouldHandlePinStateChanged(bool isPullUp, PinEventTypes pinEventTypes, int expectedButtonDownCounts, int expectedButtonUpCounts)
+        [InlineData(true, PinEventTypes.Falling, 1, 0)]
+        [InlineData(true, PinEventTypes.Rising, 0, 0)]
+        public void ShouldHandlePinStateChanged_OneWay(bool isPullUp, PinEventTypes pinEventTypes, int expectedButtonDownCounts, int expectedButtonUpCounts)
         {
             // Arrange
             var buttonDownCounter = 0;
@@ -84,6 +84,41 @@ namespace System.Gpio.Devices.Tests.Buttons
 
             // Act
             pinChangeEventHandler(button, new PinValueChangedEventArgs(pinEventTypes, ButtonPin));
+
+            // Assert
+            buttonDownCounter.Should().Be(expectedButtonDownCounts);
+            buttonUpCounter.Should().Be(expectedButtonUpCounts);
+        }
+
+        [Theory]
+        [InlineData(false, PinEventTypes.Falling, PinEventTypes.Rising, 1, 0)]
+        [InlineData(false, PinEventTypes.Rising, PinEventTypes.Falling, 1, 1)]
+        [InlineData(true, PinEventTypes.Falling, PinEventTypes.Rising, 1, 1)]
+        [InlineData(true, PinEventTypes.Rising, PinEventTypes.Falling, 1, 0)]
+        public void ShouldHandlePinStateChanged_TwoWay(bool isPullUp, PinEventTypes pinEventTypes1, PinEventTypes pinEventTypes2, int expectedButtonDownCounts, int expectedButtonUpCounts)
+        {
+            // Arrange
+            var buttonDownCounter = 0;
+            var buttonUpCounter = 0;
+
+            PinChangeEventHandler pinChangeEventHandler = null;
+            this.gpioControllerMock.Setup(c => c.RegisterCallbackForPinValueChangedEvent(ButtonPin, It.IsAny<PinEventTypes>(), It.IsAny<PinChangeEventHandler>()))
+                .Callback((int _, PinEventTypes _, PinChangeEventHandler eventHandler) => { pinChangeEventHandler = eventHandler; });
+
+            var button = new GpioButton(ButtonPin, isPullUp, gpio: this.gpioControllerMock.Object);
+
+            button.ButtonDown += (sender, e) =>
+            {
+                Interlocked.Increment(ref buttonDownCounter);
+            };
+            button.ButtonUp += (sender, e) =>
+            {
+                Interlocked.Increment(ref buttonUpCounter);
+            };
+
+            // Act
+            pinChangeEventHandler(button, new PinValueChangedEventArgs(pinEventTypes1, ButtonPin));
+            pinChangeEventHandler(button, new PinValueChangedEventArgs(pinEventTypes2, ButtonPin));
 
             // Assert
             buttonDownCounter.Should().Be(expectedButtonDownCounts);
