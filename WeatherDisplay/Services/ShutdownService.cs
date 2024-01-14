21b5df24b79e36ec60d2813eebde75b531c8c73f
Thomas Galliker <thomas@bluewin.ch>
2024-01-14 19:24:41 +0100

    Add delay before shutting down to make sure the display is clear

--- WeatherDisplay/Services/ShutdownService.cs
+++ WeatherDisplay/Services/ShutdownService.cs
@@ -1,30 +1,43 @@
-﻿using DisplayService.Services;
+﻿using System.Threading.Tasks;
+using DisplayService.Services;
+using Microsoft.Extensions.Logging;
 
 namespace WeatherDisplay.Services
 {
     public class ShutdownService : IShutdownService
     {
+        private readonly ILogger<ShutdownService> logger;
         private readonly RaspberryPi.Services.IShutdownService shutdownService;
         private readonly IDisplayManager displayManager;
 
         public ShutdownService(
+            ILogger<ShutdownService> logger,
             RaspberryPi.Services.IShutdownService shutdownService,
             IDisplayManager displayManager)
         {
+            this.logger = logger;
             this.shutdownService = shutdownService;
             this.displayManager = displayManager;
         }
 
-        public void Shutdown()
+        public async void Shutdown()
         {
+            this.logger.LogDebug("Shutdown");
+
             this.displayManager.Reset();
+
+            await Task.Delay(3000);
 
             this.shutdownService.Shutdown();
         }
 
-        public void Reboot()
+        public async void Reboot()
         {
+            this.logger.LogDebug("Reboot");
+
             this.displayManager.Reset();
+
+            await Task.Delay(3000);
 
             this.shutdownService.Reboot();
         }
